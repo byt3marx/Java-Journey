fe353ec5c0ad6f77aee475eac2298fe22a7bdda9
Marx <byt3marx@gmail.com>
2026-02-27 11:22:41 +0100

    Moved Welcome & Human exercises to the main Java repository

---
+++ 01-Basics/Human.java
@@ -0,0 +1,21 @@
+class Human {
+  public static void main(String[] args) {
+
+    String name = "Sasha";
+    int age = 27;
+    double height = 1.73;
+    boolean isStudent = false;
+    int favouriteNumber = 7;
+    String country = "Slovenia";
+    boolean isLearningJava = false;
+
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Height: " + height);
+    System.out.println("Student: " + isStudent);
+    System.out.println("Favourite Number: " + favouriteNumber);
+    System.out.println("Country: " + country);
+    System.out.println("Learning Java: " + isLearningJava);
+
+  }
+}
---
+++ 01-Basics/welcome.java
@@ -0,0 +1,7 @@
+class Welcome {
+    public static void main(String[] args) {
+        System.out.println("My name is byt3marx");
+        System.out.println("I am learning Java.");
+        System.out.println("Java is fun.");
+    }
+}
