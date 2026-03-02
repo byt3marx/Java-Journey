a58b72f4a1ae44fb153ecf7a4ff7d14acfff386e
Marx <byt3marx@gmail.com>
2026-03-02 14:39:27 +0100

    spelling correction

--- 01-Basics/human1.java
+++ 01-Basics/human1.java
@@ -10,19 +10,19 @@ class human1 {
 
     System.out.println("Enter your age: ");
     int age = scanner.nextInt();
-    scanner.nextLine();
+    scanner.nextLine(); //important line, if not inserted the program will read the leftover newline created by pressing the enter key and skip over the color entry.//
 
-    System.out.println("Enter your favourite color: ");
+    System.out.println("Enter your favorite color: ");
     String color = scanner.nextLine();
 
     System.out.println("Enter your height: ");
     double height = scanner.nextDouble();
 
-    System.out.println("Enter your favourite number: ");
+    System.out.println("Enter your favorite number: ");
     int favNumber = scanner.nextInt();
 
-    System.out.println("Hello " + name + ", you are " + age + " years old and your favourite color is " + color + ".");
-    System.out.println("You are " + height + " cm tall, and your favourite number is " + favNumber + ".");
+    System.out.println("Hello " + name + ", you are " + age + " years old and your favorite color is " + color + ".");
+    System.out.println("You are " + height + " cm tall, and your favorite number is " + favNumber + ".");
     
     scanner.close();
   }
