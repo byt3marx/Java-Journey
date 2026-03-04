9a828884ce5ab349a350ffe5ac6fe3aa76159f1e
Marx <byt3marx@gmail.com>
2026-03-04 12:27:51 +0100

    Added enum for Operations and edited the code accordingly.

--- 02-Interactive-Logic/smartCalculatorRefactor.java
+++ 02-Interactive-Logic/smartCalculatorRefactor.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 
+enum Operation {
+  ADD,
+  SUBTRACT,
+  MULTIPLY,
+  DIVIDE,
+  MODULO;
+}
+
 class SmartCalculatorRefactor {
 
   public static double getNumber(Scanner scanner, String prompt) { //gets the number from user
@@ -7,33 +15,43 @@ class SmartCalculatorRefactor {
     return scanner.nextDouble();
   }
 
-  public static String getOperation(Scanner scanner) {
+  public static Operation getOperation(Scanner scanner) {
       while (true) {
       System.out.println("Choose operation (+, -, *, /, %): ");
-      String op = scanner.next();
+      String input = scanner.next();
 
-      if ("+-*/%".contains(op) && op.length() == 1) {
-        return op;
+      switch (input) {
+        case "+":
+          return Operation.ADD;
+        case "-":
+          return Operation.SUBTRACT;
+        case "*":
+          return Operation.MULTIPLY;
+        case "/":
+          return Operation.DIVIDE;
+        case "%":
+          return Operation.MODULO;
+        default:
+          System.out.println("Invalid operation. Try again.");
       }
-      System.out.println("Invalid operation. Try again.");
     }
   }
 
-  public static double calculate(double num1, double num2, String op) { //does the calculation
+  public static double calculate(double num1, double num2, Operation op) { //does the calculation
     switch (op) {
-      case "+":
+      case ADD:
         return num1 + num2;
-      case "-":
+      case SUBTRACT:
         return num1 - num2;
-      case "*":
+      case MULTIPLY:
         return num1 * num2;
-      case "/":
+      case DIVIDE:
         if (num2 == 0) {
           System.out.println("Cannot divide by zero.");
           return Double.NaN;
         } 
         return num1 / num2;
-      case "%":
+      case MODULO:
         return num1 % num2;
         default: 
           return 0;
@@ -60,7 +78,7 @@ class SmartCalculatorRefactor {
 
     while(answer.equals("yes")) {
       double num1 = getNumber(scanner, "Enter first number: ");
-      String op = getOperation(scanner);
+      Operation op = getOperation(scanner);
       double num2 = getNumber(scanner, "Enter second number: ");
 
       double result = calculate(num1, num2, op);
