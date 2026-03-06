b0d7df8cc4fd145ad2b3f0f5e86307106df3e708
Marx <byt3marx@gmail.com>
2026-03-06 12:01:18 +0100

    Starting new learning project. Created Student class and StudentApp class. Created two Student objects.

---
+++ 03-OOP-Foundations/Student/StudentApp.java
@@ -0,0 +1,11 @@
+class StudentApp {
+
+  public static void main(String[] args) {
+
+    Student s1 = new Student("Anna", 20);
+    Student s2 = new Student("Mark", 22);
+
+    System.out.println(s1);
+    System.out.println(s2);
+  }
+}
