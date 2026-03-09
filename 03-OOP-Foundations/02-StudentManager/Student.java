9767abe80537a589898202e48d233c121551093b
Marx <byt3marx@gmail.com>
2026-03-09 07:48:00 +0100

    feat: add class, add getters & setters(with validation), add toString for printing

---
+++ 03-OOP-Foundations/02-StudentManager/Student.java
@@ -0,0 +1,37 @@
+class Student {
+
+  private String name;
+  private int age;
+
+  public Student(String name, int age) {
+    setName(name); //important to use setName instead of this.name = name, because the constructor also uses validation
+    setAge(age);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setName(String name) {
+    if (name != null && !name.isBlank()) {
+      this.name = name;
+    } else {
+      System.out.println("Invalid name.");
+    }
+  }
+
+  public void setAge(int age) {
+    if (age >= 0 || age <= 120) {
+      this.age = age;
+    } else {
+      System.out.println("Invalid age.")
+    }
+  }
+  public String toString() {
+    return name + " (" + age + ") ";
+  }
+}
