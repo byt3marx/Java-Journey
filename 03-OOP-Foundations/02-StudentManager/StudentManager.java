426885feee5e572142bf87a847b4fdd72e4f77d8
Marx <byt3marx@gmail.com>
2026-03-09 14:21:12 +0100

    feat: add StudentManager class
    feat: add constructor
    
    add methods for addStudent, showStudent & removeStudent

--- 03-OOP-Foundations/02-StudentManager/StudentManager.java
+++ 03-OOP-Foundations/02-StudentManager/StudentManager.java
@@ -1,3 +1,37 @@
+import java.util.ArrayList;
+
 class StudentManager {
-  
+
+  private ArrayList<Student> students;
+
+  public StudentManager() {
+    students = new ArrayList<>();
+  }
+
+  public void addStudent(String name, int age) {
+    Student student = new Student(name, age);
+    students.add(student);
+    System.out.println("Student added.");
+  }
+
+  public void showStudent() {
+    if(students.isEmpty()) {
+      System.out.println("No students in the list.");
+      return;
+    }
+
+    for (int i = 0; i < students.size(); i++) {
+      System.out.println((i + 1) + ". " + students.get(i));
+    }
+  }
+
+  public void removeStudent(int index) {
+    if (index < 0 || index >= students.size()) {
+      System.out.println("Invalid student number.");
+      return;
+    }
+
+    Student removedStudent = students.remove(index);
+    System.out.println("Removed: " + removedStudent);
+  }
 }
