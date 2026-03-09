f9c368a92f82a680486611fa7307ee6c021c0b84
Marx <byt3marx@gmail.com>
2026-03-09 05:40:04 +0100

    test: test printing age with negative input, create new student & use setter to change students info.

--- 03-OOP-Foundations/Student/StudentApp.java
+++ 03-OOP-Foundations/Student/StudentApp.java
@@ -4,10 +4,21 @@ class StudentApp {
 
     Student s1 = new Student("Anna", 20);
     Student s2 = new Student("Mark", 22);
+    Student s3 = new Student("Igor", 5);
 
     System.out.println(s1.getName());
     System.out.println(s1.getAge());
     System.out.println(s2.getName());
     System.out.println(s2.getAge());
+
+    s1.setAge(-10);
+    System.out.println(s2.getAge());
+
+    s3.setName("Rok");
+    s3.setAge(25);
+    System.out.println(s3.getName());
+    System.out.println(s3.getAge());
+
+    
   }
 }
