9da6ac4db0d87d28a94f7a44bd6a8766c420d54a
Marx <byt3marx@gmail.com>
2026-03-10 08:59:36 +0100

    feat: create Main to run the program
    add input reader, manager object and loop control variable
    
    add menu for user interaction, loop while running
    
    add switch for four cases: addStudent, showStudent, removeStudent & exit
    
    add NumberFormatException for parsing input to int

---
+++ 03-OOP-Foundations/02-StudentManager/Main.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+class Main {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    StudentManager manager = new StudentManager();
+
+    boolean running = true;
+
+    while (running) {
+      System.out.println("\n--- Student Manager ---");
+      System.out.println("1. Add student");
+      System.out.println("2. Show students");
+      System.out.println("3. Remove student");
+      System.out.println("4. Exit");
+      System.out.println("Choose option: ");
+
+      String input = scanner.nextLine();
+
+      switch (input) {
+        case "1":
+          System.out.println("Enter student name: ");
+          String name = scanner.nextLine();
+
+          System.out.println("Enter student age: ");
+          String ageInput = scanner.nextLine();
+
+          try {
+            int age = Integer.parseInt(ageInput);
+            manager.addStudent(name, age);
+          } catch (NumberFormatException e) {
+            System.out.println("Invalid age. Please enter a whole number.");
+          }
+          break;
+        
+        case "2":
+          manager.showStudent();
+          break;
+
+        case "3":
+          manager.showStudent();
+
+          System.out.println("Enter student number to remove: ");
+          String removeInput = scanner.nextLine();
+
+          try {
+            int studentNumber = Integer.parseInt(removeInput);
+            manager.removeStudent(studentNumber -1);
+          } catch (NumberFormatException e) {
+            System.out.println("Invalid number.");
+          }
+          break;
+
+        case "4":
+          running = false;
+          System.out.println("Program ended.");
+          break;
+
+        default:
+          System.out.println("Invalid option.");
+      }
+    }
+
+    scanner.close();
+  }
+}
