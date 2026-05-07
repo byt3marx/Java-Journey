273c13a1d7fbbb2a7bc321acb647ad08e666e1b4
Marx <byt3marx@gmail.com>
2026-05-07 13:26:08 +0200

    test:  div has block children(multiline) and second parahraph has only inline children(one line)

--- 05-Projects/02-JsonToHtmlParser/src/app/Main.java
+++ 05-Projects/02-JsonToHtmlParser/src/app/Main.java
@@ -45,8 +45,37 @@ public class Main {
 
         String result1 = builder.buildHtml(paragraph, 0);
 
+        Map<String, Object> p3 = new HashMap<>();
+        p3.put("tag", "p");
+        p3.put("text", "Hello");
+
+        Map<String, Object> em = new HashMap<>();
+        em.put("tag", "em");
+        em.put("text", "example");
+
+        List<Object> p4Children = new ArrayList<>();
+        p4Children.add("This is an ");
+        p4Children.add(em);
+        p4Children.add("text.");
+
+        Map<String, Object> p4 = new HashMap<>();
+        p4.put("tag", "p");
+        p4.put("children", p4Children);
+
+        List<Object> divChildren = new ArrayList<>();
+        divChildren.add(p3);
+        divChildren.add(p4);
+
+        Map<String, Object> div1 = new HashMap<>();
+        div1.put("tag", "div");
+        div1.put("children", divChildren);
+
+        String result2 = builder.buildHtml(div1, 0);
+
         System.out.println(result);
 
         System.out.println(result1);
+
+        System.out.println(result2);
     }
 }
