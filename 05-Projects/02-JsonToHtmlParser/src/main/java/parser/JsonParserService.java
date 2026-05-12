27f7bb4f582eff9e81328e1ca5d62a69bb7ab12a
Marx <byt3marx@gmail.com>
2026-05-12 07:02:51 +0200

    feat: deserialize JSON strings into Map structures using Gson

--- 05-Projects/02-JsonToHtmlParser/src/main/java/parser/JsonParserService.java
+++ 05-Projects/02-JsonToHtmlParser/src/main/java/parser/JsonParserService.java
@@ -1,4 +1,13 @@
 package parser;
 
+import com.google.gson.Gson;
+import java.util.Map;
+
 public class JsonParserService {
+
+    private final Gson gson = new Gson();
+
+    public Map<String, Object> parseJson(String json) {
+        return gson.fromJson(json, Map.class);
+    }
 }
