52f7461e38ce978dddca33df00b53875fdca4159
Marx <byt3marx@gmail.com>
2026-05-15 10:19:53 +0200

    refactor: replace wildcard imports with explicit imports

--- 05-Projects/02-JsonToHtmlParser/src/main/java/parser/JsonToHtmlNodeConverter.java
+++ 05-Projects/02-JsonToHtmlParser/src/main/java/parser/JsonToHtmlNodeConverter.java
@@ -1,6 +1,10 @@
 package parser;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class JsonToHtmlNodeConverter {
 
