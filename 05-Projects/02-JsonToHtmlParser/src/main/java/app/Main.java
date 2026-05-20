e6825f306d027ea156dfcb69f72a2a3730411e83
Marx <byt3marx@gmail.com>
2026-05-20 12:55:02 +0200

    test Exception handeling

--- 05-Projects/02-JsonToHtmlParser/src/main/java/app/Main.java
+++ 05-Projects/02-JsonToHtmlParser/src/main/java/app/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String inputPath = "src/main/resources/input/";
+        String inputPath = "src/main/resources/input/helloWorld.json";
         String outputPath = "src/main/resources/output/helloWorld.html";
 
         JsonLoader loader = new JsonLoader();
