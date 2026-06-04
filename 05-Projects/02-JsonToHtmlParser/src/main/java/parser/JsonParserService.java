3a4a31aa6097791c69e15d9a974784749b2a5146
Marx <byt3marx@gmail.com>
2026-06-04 16:56:21 +0200

    refactor: use TypeToken for JSON parsing

--- 05-Projects/02-JsonToHtmlParser/src/main/java/parser/JsonParserService.java
+++ 05-Projects/02-JsonToHtmlParser/src/main/java/parser/JsonParserService.java
@@ -1,6 +1,8 @@
 package parser;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
 import java.util.Map;
 
 public class JsonParserService {
@@ -14,8 +16,10 @@ public class JsonParserService {
         }
 
         try {
-            return gson.fromJson(json, Map.class);
-        } catch (Exception e) {
+            Type mapType = new TypeToken<Map<String, Object>>() {}.getType();
+            return gson.fromJson(json, mapType);
+        }
+        catch (Exception e) {
             throw new IllegalArgumentException("Failed to parse JSON", e);
         }
     }
