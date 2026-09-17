fad538100e73bb23e018c5dc9f302f5e4345026d
Marx <byt3marx@gmail.com>
2026-09-17 14:45:49 +0200

    chore: initialize Spring Boot project

---
+++ 05-Projects/05-TaskManagementApi/src/test/java/com/marx/task/api/TaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marx.task.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
